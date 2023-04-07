5d158eaa1f3f9e620141230da328c8958e696de8
GrungeElFz <elf0114_@hotmail.com>
2023-04-07 00:12:52 -0500

    Modified:   Added navigation to 'About' page (src/routes/+page.svelte)
    Modified:   Added navigation to 'Home' page (src/routes/about/+page.svelte)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script>
+	import { goto } from '$app/navigation';
 	import { Button } from '$components';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button
+	on:click={() => {
+		goto('/about');
+	}}>Go to About</button
+>
 
 <Button />
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,11 @@
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
 <h1>About</h1>
+
+<button
+	on:click={() => {
+		goto('/');
+	}}>Go to Home</button
+>
